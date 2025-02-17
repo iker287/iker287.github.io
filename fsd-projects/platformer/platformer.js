43788b0bddecdff0c99f2f68b4412ccf8dd6fd87
iker287 <iker.cerrato@jpschools.org>
2025-02-17 17:43:20 +0000

    saving project

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -18,37 +18,52 @@ $(function () {
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
-    createPlatform(-50, -50, 50, canvas.height + 500); // left wall
-    createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
-
-    //////////////////////////////////
-    // ONLY CHANGE BELOW THIS POINT //
-    //////////////////////////////////
-
-    // TODO 1 - Enable the Grid
-    // toggleGrid();
+   createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
+   createPlatform(-50, -50, 50, canvas.height + 500); // left wall
+   createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
 
-    // TODO 2 - Create Platforms
+   //////////////////////////////////
+   // ONLY CHANGE BELOW THIS POINT //
+   //////////////////////////////////
 
+
+   // TODO 1 - Enable the Grid
+   toggleGrid();
 
 
 
-    // TODO 3 - Create Collectables
+
+   // TODO 2 - Create Platforms
+   createPlatform(400,500 ,200, 300,"purple");
 
 
-
-    
-    // TODO 4 - Create Cannons
+   createPlatform(200, 620, 100, 20,"purple");
 
 
-    
-    
-    //////////////////////////////////
-    // ONLY CHANGE ABOVE THIS POINT //
-    //////////////////////////////////
-  }
+   createPlatform(400, 405, 400, 10,"purple");
+  
+   createPlatform(900, 620, 200, 20,"purple");
+   createPlatform(1000, 500, 100, 20,"purple");
+   createPlatform(900, 300, 500, 20,"purple");
+   createPlatform(850, 450, 100, 20,"purple");
+   // TODO 3 - Create Collectables
+   createCollectable("diamond", 200, 170, 0.5, 0.7);
+   createCollectable("diamond", 700, 700, 0.5, 0.7);
+   createCollectable("steve", 1350, 50);
+  
+   // TODO 4 - Create Cannons
+   createCannon("top", 260, 1000);
+   createCannon("right", 500, 1500);
+   createCannon("right", 700, 2500);
+   createCannon("top", 100, 100);
+   //////////////////////////////////
+   // ONLY CHANGE ABOVE THIS POINT //
+   //////////////////////////////////
+ }
 
-  registerSetup(setup);
+
+ registerSetup(setup);
 });
+
+
